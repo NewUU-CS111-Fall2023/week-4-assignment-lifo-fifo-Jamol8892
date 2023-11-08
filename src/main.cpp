8553a3c2e4f3a99b43b55a7d679b0d77c4077173
Jamol <127374162+Jamol8892@users.noreply.github.com>
2023-11-08 22:33:12 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,31 @@
-/*
- * Author:
- * Date:
- * Name:
- */
-
 #include <iostream>
 #include "task_1.h"
+#include "task_3.h"
+#include "task_4.h"
 
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+
+    cout << "Problem 1:" << endl;
+    Stack stack;
+
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    stack.display();
+
+    cout << "Top element: " << stack.peek() << endl;
+
+    stack.pop();
+
+    stack.display();
+
+    cout << "Problem 3: " << endl;
+    problem3();
+
+    cout << "Problem 4: " << endl;
+    problem4();
+
     return 0;
 }
+
