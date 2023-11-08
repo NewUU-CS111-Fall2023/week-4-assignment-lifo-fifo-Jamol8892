e2e1fbe208b8209985d567706beffbedba669301
Jamol <127374162+Jamol8892@users.noreply.github.com>
2023-11-08 22:33:25 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,58 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+class Stack {
+private:
+    Node* top;
+
+public:
+    Stack() { top = NULL; }
+
+    void push(int item) {
+        Node* newNode = new Node;
+        newNode->data = item;
+        newNode->next = top;
+        top = newNode;
+    }
+
+    int pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+
+        int item = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        return item;
+    }
+
+    int peek() {
+        if (isEmpty()) {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        return top->data;
+    }
+
+    bool isEmpty() {
+        return top == NULL;
+    }
+
+    void display() {
+        cout << "Stack: ";
+        Node* temp = top;
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
